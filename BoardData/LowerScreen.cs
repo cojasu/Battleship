61d0d3dd4b05c841eaf3feae67b6ed3c6aeb6d2b
cojasu <cjs110@zips.uakron.edu>
2014-11-14 21:39:40 -0500

    Updated lowerScreen

--- BoardData/LowerScreen.cs
+++ BoardData/LowerScreen.cs
@@ -8,5 +8,67 @@ namespace BattleshipTest.BoardData
 {
     public class LowerScreen
     {
+        int[,] screen = new int[10, 10];
+
+        public LowerScreen()
+        {
+            for (int i = 0; i < screen.GetLength(0); i++)
+            {
+                for (int j = 0; j < screen.GetLength(1); j++)
+                {
+                    screen[i, j] = 0;
+                }
+            }
+
+        }
+
+        public void printExtended()
+        {
+            for (int i = 0; i < screen.GetLength(0); i++)
+            {
+                for (int j = 0; j < screen.GetLength(1); j++)
+                {
+                    Console.Write("upperPlayScreen: [" + i + "][" + j + "]: " + numToString(screen[i, j]) + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public void printSimple()
+        {
+            for (int i = 0; i < screen.GetLength(0); i++)
+            {
+                for (int j = 0; j < screen.GetLength(1); j++)
+                {
+                    Console.Write(numToString(screen[i, j]) + " " );
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public string numToString(int num)
+        {
+            if (num == 0)
+            {
+                //no data
+                return "ND";
+            }
+            if (num == 1)
+            {
+                //hit
+                return "H";
+            }
+            if (num == 2)
+            {
+                //miss
+                return "M";
+            }
+            else
+            {
+                //error
+                return "E";
+            }
+        }
+    }
     }
 }
